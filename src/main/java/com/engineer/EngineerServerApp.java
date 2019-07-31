45541ec4fa86d6df9b2a7ce41a0e82d5153ffdeb
Mateusz Karwacki <mateusz.karwacki@onwelo.com>
2019-07-31 09:19:30 +0200

    saa

--- src/main/java/com/engineer/EngineerServerApp.java
+++ src/main/java/com/engineer/EngineerServerApp.java
@@ -14,13 +14,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //@EnableJpaRepositories
 //@EntityScan("com.engineer")
 
-public class EngineerServerApp {
+public class EngineerServerApp extends SpringBootServletInitializer {
         public static void main(String[] args) {
             SpringApplication.run(EngineerServerApp.class, args);
         }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(EngineerServerApp.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EngineerServerApp.class);
+    }
 }
